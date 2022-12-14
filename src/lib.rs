bc839bb29df4d223a26554898287d39ca83f2e97
gavlig <gavlig@gmail.com>
2022-12-15 00:03:10 +0100

    fix jitter in amount of visible rows when moving camera fast

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,8 @@ impl Plugin for ReaderCameraPlugin {
 			.add_system(systems::mouse_fly)
 			.add_system(systems::mouse_follow)
 			.add_system(systems::mouse_reader)
-			.add_system(systems::calc_visible_rows)
+			
+			.add_system_to_stage(CoreStage::PreUpdate, systems::calc_visible_rows) // PreUpdate because Frustum gets desynced with camera transform and that makes the amount of visible rows jitter
 		;
 	}
 }
