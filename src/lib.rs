5db4450ff5cabbffe70fa296ed3aa14e09376cac
gavlig <gavlig@gmail.com>
2023-01-04 23:41:15 +0100

    make sure camera is updated before everything else to always have relevant transform for other calculations

--- src/lib.rs
+++ src/lib.rs
@@ -35,8 +35,8 @@ impl Plugin for ReaderCameraPlugin {
 			.add_system(systems::keyboard_fly)
 			.add_system(systems::mouse_fly)
 			.add_system(systems::mouse_follow)
-			.add_system(systems::mouse_reader)
 			
+			.add_system_to_stage(CoreStage::PreUpdate, systems::mouse_reader)
 			.add_system_to_stage(CoreStage::PreUpdate, systems::calc_visible_rows) // PreUpdate because Frustum gets desynced with camera transform and that makes the amount of visible rows jitter
 		;
 	}
